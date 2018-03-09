d6ea7439c461291659f0cbacf157dfb50dc54f79
HeNdRa BiXaS <37101779+grenden@users.noreply.github.com>
2018-03-09 10:32:44 +0900

    Add files via upload

---
+++ simi.py
@@ -0,0 +1,11 @@
+import urllib, json
+
+print "Silahkan kirim pesan!"
+
+while(True):
+    pesan = raw_input("Anda: ")
+    url = "http://sandbox.api.simsimi.com/request.p?key=f21a9486-4cc3-43a7-a212-43b83d2f287d&lc=id&ft=1.0&text=%s" % pesan
+    link_json = urllib.urlopen(url)
+    data = json.loads(link_json.read())
+
+    print "Petanikode-simi: %s" % data['response']
